cb226e8df01a95145ed175287a8f8a0aa0937424
Pit_killer2.1 <brad.alvarez@epn.edu.ec>
2017-06-06 17:18:51 -0500

    Ejercicio 9 y 10

--- EjerciciosTkinter.py
+++ EjerciciosTkinter.py
@@ -89,3 +89,35 @@ Checkbutton(master, text="female", variable=var2).grid(row=2, sticky=W)
 Button(master, text='Quit', command=master.quit).grid(row=3, sticky=W, padx=4)
 Button(master, text='Show', command=var_states).grid(row=4, sticky=W, pady=4)
 mainloop()
+
+#Ejercicio 9
+
+from Tkinter import *
+master = Tk()
+Label(master, text="First Name").grid(row=0)
+Label(master, text="Last Name").grid(row=1)
+e1 = Entry(master)
+e2 = Entry(master)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+mainloop()
+
+#Ejercicio 10
+
+from Tkinter import *
+def show_entry_fields():
+    print("First Name: %s\nLast Name: %s" % (e1.get(), e2.get()))
+
+master = Tk()
+Label(master, text="First Name").grid(row=0)
+Label(master, text="Last Name").grid(row=1)
+
+e1 = Entry(master)
+e2 = Entry(master)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, pady=4)
+Button(master, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+mainloop()
